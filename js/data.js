d5239d12caa9e87c1c5c70739e54fc56c71275fa
Hamza Bouzid <hamzadeveloper1@gmail.com>
2022-01-12 16:09:36 +0100

    completato + bonus colori

--- js/data.js
+++ js/data.js
@@ -115,13 +115,11 @@ const dati = [
 
 // Funzione per generare i colori in modo dinamico
 function colorGenerator(length) {
-  let result           = '';
-  let characters       = 'ABCDEF0123456789';
+  let result = "";
+  let characters = "ABCDEF0123456789";
   let charactersLength = characters.length;
-  for ( var i = 0; i < length; i++ ) {
-    result += characters.charAt(Math.floor(Math.random() * 
-charactersLength));
- }
- return result;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
 }
-
